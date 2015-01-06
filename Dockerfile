8b3c329cfc81c678b418a7d2898078aeb6a17159
xcezx <main.xcezx@gmail.com>
2015-01-07 04:10:30 +0900

    wip

---
+++ Dockerfile
@@ -0,0 +1,24 @@
+FROM centos:centos6
+MAINTAINER xcezx <main.xcezx@gmail.com>
+
+ENV MYSQL_VERSION 5.6.22
+ENV MYSQL_ROOT_PASSWORD p4ssw0rd
+ENV Q4M_VERSION 0.9.14
+ENV TZ Asia/Tokyo
+
+WORKDIR /tmp/workdir
+
+RUN yum -y install curl tar gcc gcc-c++ cmake ncurses-devel libaio-devel perl && \
+    yum -y clean all
+
+RUN groupadd -r mysql && useradd -r -g mysql mysql
+RUN curl -s -L https://github.com/kamipo/mysql-build/archive/master.tar.gz | tar vzx
+
+RUN mkdir -p /usr/local/mysql
+VOLUME /usr/local/mysql
+
+COPY docker-entrypoint.sh /entrypoint.sh
+ENTRYPOINT ["/entrypoint.sh"]
+
+EXPOSE 3306
+CMD ["/usr/local/mysql/bin/mysqld_safe", "--user=mysql"]
---
+++ docker-entrypoint.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+set -e
+
+rm -f /etc/localtime
+cp -p /usr/share/zoneinfo/$TZ /etc/localtime
+
+if [ ! -d "/usr/local/mysql/data" ]; then
+	./mysql-build-master/bin/mysql-build --verbose $MYSQL_VERSION /usr/local/mysql q4m-$Q4M_VERSION
+	cd /usr/local/mysql
+	chown -R mysql:mysql .
+	./scripts/mysql_install_db --user=mysql
+	chown -R root .
+	chown -R mysql data
+
+	TMPFILE=$(mktemp)
+        chmod 0666 $TMPFILE
+	cat > $TMPFILE <<-EOSQL
+		DELETE FROM mysql.user ;
+		CREATE USER 'root'@'%' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD' ;
+		GRANT ALL ON *.* TO 'root'@'%' WITH GRANT OPTION ;
+		DROP DATABASE IF EXSITS test ;
+	EOSQL
+
+	if [ "$MYSQL_DATABASE" ]; then
+		echo "CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE ;" >> $TMPFILE
+	fi
+
+	if [ "$MYSQL_USER" -a "$MYSQL_USER_PASSWORD" ]; then
+		echo "CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_USER_PASSWORD' ;" >> $TMPFILE
+		if [ "$MYSQL_DATABASE" ]; then
+			echo "GRANT ALL ON $MYSQL_DATABASE.* TO '$MYSQL_USER'@'%' WITH GRANT OPTION ;" >> $TMPFILE
+		fi
+	fi
+
+	echo "FLUSH PRIVILEGES ;" >> $TMPFILE
+	cat /usr/local/mysql/support-files/install-q4m.sql >> $TMPFILE
+
+	set -- "$@" --init-file=$TMPFILE
+fi
+
+exec "$@"
